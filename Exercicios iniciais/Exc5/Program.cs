4f0078d8ed9f139a29d808c95338deb32533cc8d
Lucas Andrusievicz <lucasandru@gmail.com>
2024-09-21 22:28:15 -0300

    Add files via upload

---
+++ Exercicios iniciais/Exc5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double numero1, numero2, soma, subtracao, multiplicacao, divisao;
+
+        Console.WriteLine("Digite o primeiro numero:");
+        numero1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo numero:");
+        numero2 = double.Parse(Console.ReadLine());
+
+        soma = numero1 + numero2;
+        subtracao = numero1 - numero2;
+        multiplicacao = numero1 * numero2;
+        divisao = numero1 / numero2;
+
+        Console.WriteLine($"A soma dos numeros é: {soma:F2}");
+        Console.WriteLine($"A subtracao dos numeros é: {subtracao:F2}");
+        Console.WriteLine($"A multiplicacao dos numeros é: {multiplicacao:F2}");
+        Console.WriteLine($"A divisao dos numeros é: {divisao:F2}");
+    }
+}
