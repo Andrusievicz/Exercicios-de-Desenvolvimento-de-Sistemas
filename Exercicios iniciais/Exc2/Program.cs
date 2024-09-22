c97b6e825034ab871b93f76447fc30a58000f768
Lucas Andrusievicz <lucasandru@gmail.com>
2024-09-21 22:27:01 -0300

    Add files via upload

---
+++ Exercicios iniciais/Exc2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o salario base do vendedor:");
+        double salarioBase = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o total de vendas do vendedor:");
+        double totalVendas = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a porcentagem de comissao (em %):");
+        double percentualComissao = double.Parse(Console.ReadLine());
+
+        double comissao = (percentualComissao / 100) * totalVendas;
+
+        double salarioTotal = salarioBase + comissao;
+
+        Console.WriteLine($"Salario base: {salarioBase:C}");
+        Console.WriteLine($"Comissao: {comissao:C}");
+        Console.WriteLine($"Salario total: {salarioTotal:C}");
+    }
+}
