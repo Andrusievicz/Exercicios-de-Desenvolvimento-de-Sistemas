427ad9dce6903ef6b7e94208a2f46718cc76db2b
Lucas Andrusievicz <lucasandru@gmail.com>
2024-09-23 19:03:53 -0300

    Add files via upload

---
+++ EXERCICIOS AULA - 22 23 - Tratamento de exceções/EXERCICIO 1 AULA - 22 23/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Solicita que o usuário digite um número inteiro
+            Console.Write("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            // Realiza a divisão de 100 pelo número fornecido
+            int resultado = 100 / numero;
+
+            // Exibe o resultado
+            Console.WriteLine($"100 Dividido por {numero} é {resultado}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Erro: Não é possivel dividir por zero.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erro: Entrada inválida. Por favor digite um número inteiro.");
+        }
+        catch (Exception ex)
+        {
+            // Trata qualquer outra exceção que não tenha sido capturada
+            Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+        }
+    }
+}
---
+++ EXERCICIOS AULA - 22 23 - Tratamento de exceções/EXERCICIO 2 AULA - 22 23/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+// Exceção personalizada para idade inválida
+public class InvalidAgeException : Exception
+{
+    public InvalidAgeException(string message) : base(message)
+    {
+
+    }
+}
+
+class Program
+{
+    // Função que verifica se a idade está dentro de um intervalo válido
+    static void VerificarIdade(int idade)
+    {
+        if (idade < 0 || idade > 120)
+        {
+            // Lança a exceção personalizada se a idade estiver fora do intervalo
+            throw new InvalidAgeException($"Idade inválida: {idade}. A idade deve estar entre 0 e 120.");
+        }
+        else
+        {
+            Console.WriteLine($"Idade válida: {idade}");
+        }
+    }
+
+    static void Main()
+    {
+        try
+        {
+            // Solicita que o usuário insira uma idade
+            Console.Write("Digite a idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            // Chama a função para verificar a idade
+            VerificarIdade(idade);
+        }
+        catch (InvalidAgeException ex)
+        {
+            // Captura e trata a exceção personalizada
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+        catch (FormatException ex)
+        {
+            // Captura e trata entradas inválidas
+            Console.WriteLine("Erro: Entrada inválida. Por favor, digite um número inteiro");
+        }
+        catch (Exception ex)
+        {
+            // Captura qualquer outra exceção não tratada
+            Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+        }
+    }
+}
