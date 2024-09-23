22256cda3b6ae5d6e59716b42cc3d9bc41095615
Lucas Andrusievicz <lucasandru@gmail.com>
2024-09-23 18:57:25 -0300

    Add files via upload

---
+++ Exercicios aula 3/Aula3Exc4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita ao usuário que insira uma frase
+        Console.Write("Digite uma frase:");
+        string frase = Console.ReadLine();
+
+        // Calcula o número de caracteres na frase
+        int num_caracteres = frase.Length;
+
+        // Converte a frase para maiúsculas
+        string frase_maiuscula = frase.ToUpper();
+
+        // Inverte a frase
+        char[] array_caracteres = frase.ToCharArray();
+        Array.Reverse(array_caracteres);
+        string frase_invertida = new string(array_caracteres);
+
+        // Exibe os resultados
+        Console.WriteLine($"Numero de caracteres: {num_caracteres}");
+        Console.WriteLine($"Frase maiúscula: {frase_maiuscula}");
+        Console.WriteLine($"Frase invertida: {frase_invertida}");
+    }
+}
---
+++ Exercicios aula 3/Aula3Exc5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita ao usuário que insira um número
+        Console.Write("Digite um numero: ");
+        double num = double.Parse(Console.ReadLine());
+
+        // Verifica se o número é par ou ímpar
+        if (num % 2 == 0)
+        {
+            Console.WriteLine($"O número {num} é par.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {num} é ímpar.");
+        }
+    }
+}
