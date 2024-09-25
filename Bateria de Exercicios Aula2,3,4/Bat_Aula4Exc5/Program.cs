2dfa5ab9eb24c3fff8098a2fcc6e36f10e21aefa
Lucas Andrusievicz <lucasandru@gmail.com>
2024-09-25 19:02:43 -0300

    Add files via upload

---
+++ Bateria de Exercicios Aula2,3,4/Bat_Aula4Exc5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita ao usuário que insira uma string
+        Console.Write("Digite uma frase: ");
+        string input = Console.ReadLine();
+
+        // Chama o método para inverter a string
+        string invertida = InverterString(input);
+
+        // Exibe a string invertida
+        Console.WriteLine($"A frase invertida é: {invertida}");
+    }
+    // Método que inverte uma string
+    static string InverterString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse( charArray );
+        return new string( charArray );
+    }
+}
